c22371573240be315f237843f7a312909dba7735
Daniel Suarez <suarez.daniel101@gmail.com>
2023-03-06 19:11:48 -0500

    Done.

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -9,18 +9,11 @@ class SecretsController < ApplicationController
 
   private
 
-  # def require_login
-  #   unless current_user
-  #     flash[:error] = "You must be logged in to access this section"
-  #     redirect_to '/sessions/new' 
-  #   end
-  # end
-
   def require_login
-    return flash[:error] = "You must be logged in to access this section" unless session.include? current_user
-  end 
+    unless current_user
+      flash[:error] = "You must be logged in to access this section"
+      redirect_to '/sessions/new' 
+    end
+  end
 
-  # def require_login
-  #   return head(:forbidden) unless session.include? current_user
-  # end 
 end
